bc64ecf2701bfb8b7ae48e71b95a646179f858b9
rachelstephi <87799664+rachelstephi@users.noreply.github.com>
2026-06-27 20:02:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+
+const output = document.getElementById("output");
+
+function getArray() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000); 
+  });
+}
+
+getArray()
+  .then((arr) => {
+    return new Promise((resolve) => {
+      const evenNumbers = arr.filter((num) => num % 2 === 0);
+
+      setTimeout(() => {
+        output.textContent = evenNumbers;
+        resolve(evenNumbers);
+      }, 1000); 
+  })
+  .then((evenNumbers) => {
+    return new Promise((resolve) => {
+      const doubled = evenNumbers.map((num) => num * 2);
+
+      setTimeout(() => {
+        output.textContent = doubled;
+        resolve(doubled);
+      }, 2000); 
+    });
+  });
